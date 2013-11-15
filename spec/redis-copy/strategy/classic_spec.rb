6f2c1f4403b7f1103be0cdb83723cf86fb282347
Ryan Biesemeyer <ryan@yaauie.com>
2013-11-15 04:51:23 +0000

    Refactor strategy spec to have shared examples that can be consumed by third-party implementations.

---
+++ spec/redis-copy/strategy/classic_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require_relative '../../spec_helper'
+
+require 'redis-copy'
+require 'redis-copy/strategy/interface.spec'
+
+describe RedisCopy::Strategy::Classic do
+  let(:selector) { :classic }
+  it_should_behave_like RedisCopy::Strategy do
+    context '#maybe_pipeline' do
+      it 'should not pipeline' do
+        source.should_not_receive(:pipelined)
+        strategy.maybe_pipeline(source) { }
+      end
+    end
+
+    context 'with pipeline enabled' do
+      let(:options) { Hash.new(pipeline: true) }
+      it_should_behave_like RedisCopy::Strategy
+      context '#maybe_pipeline' do
+        it 'should pipeline' do
+          source.should_receive(:pipelined)
+          strategy.maybe_pipeline(source) { }
+        end
+      end
+    end
+  end
+end
