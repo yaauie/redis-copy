1d77480f0acb7d18eec5c9202da6d99574b41255
Ryan Biesemeyer <ryan@yaauie.com>
2013-11-15 04:43:15 +0000

    Refactor key-emitter spec to have shared examples that can be consumed by third-party implementations.

---
+++ lib/redis-copy/key-emitter/spec.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+# The shared examples for RedisCopy::KeyEmitter are available to require
+# into consuming libraries so they can verify their implementation of the
+# RedisCopy::KeyEmitter interface. See the bundled specs for the bundled
+# key-emitters for example usage.
+if defined?(::RSpec)
+  shared_examples_for RedisCopy::KeyEmitter do
+    let(:resolved_implementation) do
+      begin
+        instance
+        true
+      rescue Implements::Implementation::NotFound
+        false
+      end
+    end
+    let(:emitter_klass) { described_class }
+    let(:redis) { Redis.new(REDIS_OPTIONS).tap(&:ping) }
+    let(:ui) { double.as_null_object }
+    let(:selector) { emitter_klass.name.underscore.dasherize } # see implements gem
+    let(:instance) { RedisCopy::KeyEmitter.implementation(selector).new(redis, ui) }
+    let(:key_count) { 1 }
+    let(:keys) { key_count.times.map{|i| i.to_s(16) } }
+
+    before(:each) do
+      unless resolved_implementation
+        pending "#{emitter_klass} not supported in your environment"
+      end
+      key_count.times.each_slice(50) do |keys|
+        kv = keys.map{|x| x.to_s(16)}.zip(keys)
+        redis.mset(*kv.flatten)
+      end
+      ui.stub(:debug).with(anything)
+    end
+    after(:each) { redis.flushdb }
+
+    context '#keys' do
+      let(:key_count) { 64 }
+      context 'the result' do
+        subject { instance.keys }
+        its(:to_a) { should =~ keys }
+      end
+    end
+
+    context 'implementation resolution' do
+      subject { instance }
+      its(:class) { should eq described_class }
+    end
+  end
+else
+  fail(LoadError,
+       "#{__FILE__} contains shared examples for RedisCopy::KeyEmitter. " +
+       "Require it in your specs, not your code.")
+end
---
+++ spec/redis-copy/key-emitter/keys_spec.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+require_relative '../../spec_helper.rb'
+
+require 'redis-copy'
+require 'redis-copy/key-emitter/spec'
+
+describe RedisCopy::KeyEmitter::Keys do
+  it_should_behave_like RedisCopy::KeyEmitter do
+    context '#keys' do
+      context 'the supplied ui' do
+        it 'should get a debug message' do
+          ui.should_receive(:debug).
+            with(/#{redis.client.id} KEYS \*/).
+            exactly(:once)
+          instance.keys
+        end
+        context 'when source has > 10,000 keys' do
+          let(:key_count) { 10_001 }
+          it 'should ask for confirmation' do
+            ui.should_receive(:confirm?) do |confirmation|
+              confirmation.should match /\b10,001/
+            end
+            instance.keys
+          end
+        end
+        context 'when source has <= 10,000 keys' do
+          let(:key_count) { 1_000 }
+          it 'should not ask for confirmation' do
+            ui.should_not_receive(:confirm?)
+            instance.keys
+          end
+        end
+      end
+    end
+  end
+end
