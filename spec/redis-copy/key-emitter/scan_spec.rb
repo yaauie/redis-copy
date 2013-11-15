ff47297adaf0d993b5da3828b6f7f170fa1ffdc5
Ryan Biesemeyer <ryan@yaauie.com>
2013-11-15 05:01:07 +0000

    be consistent

--- spec/redis-copy/key-emitter/scan_spec.rb
+++ spec/redis-copy/key-emitter/scan_spec.rb
@@ -2,7 +2,7 @@
 require_relative '../../spec_helper.rb'
 
 require 'redis-copy'
-require 'redis-copy/key-emitter/spec'
+require 'redis-copy/key-emitter/interface.spec'
 
 describe RedisCopy::KeyEmitter::Scan do
   it_should_behave_like RedisCopy::KeyEmitter
